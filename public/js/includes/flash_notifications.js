590b23925674f25a7999d6582a5a3712d5167837
Sameer-Goutam06 <r.n.v.s.sgautham@gmail.com>
2024-08-02 16:39:49 +0530

    Seperated flash notifications logic

---
+++ public/js/includes/flash_notifications.js
@@ -0,0 +1,11 @@
+document.addEventListener('DOMContentLoaded', (event) => {
+    // Check if there's a flash message element
+    const flashMessage = document.querySelector('.flash-notifications');
+    if (flashMessage) {
+        // Hide the flash message after 3 seconds
+        setTimeout(() => {
+            flashMessage.style.opacity = '0';
+            setTimeout(() => flashMessage.remove(), 100); // Remove element after fade out
+        }, 2500);
+    }
+    });
--- public/js/includes/navbar.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const toggler = document.getElementById('navbar-toggler');
-    const collapse = document.getElementById('navbar-collapse');
-
-    toggler.addEventListener('click', () => {
-        collapse.classList.toggle('show');
-    });
-});
